780a8d734243e4358157f86759c061547cf4e9c6
Atom <sarming@126.com>
2022-02-27 21:44:25 +0800

    UDF

---
+++ src/main/java/com/atom/hive/func/ToDate.java
@@ -0,0 +1,43 @@
+package com.atom.hive.func;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * 将字符串转换成日期
+ *
+ * @author Atom
+ */
+@Description(name = "to_date",
+        value = "this is my first udf !",
+        extended = "For example : select to_date('2022/02/27 12:12:12') ;")
+public class ToDate extends UDF {
+
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+
+    /**
+     * 计算
+     *
+     * @param dateTimeStr
+     * @return
+     */
+    public Date evaluate(String dateTimeStr) {
+        try {
+            LocalDateTime localDateTime = LocalDateTime.parse(dateTimeStr, dateTimeFormatter);
+            Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+            return Date.from(instant);
+        } catch (Exception e) {
+            //ignore
+        }
+        return new Date();
+    }
+
+
+}
