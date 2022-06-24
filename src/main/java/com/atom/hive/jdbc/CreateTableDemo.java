b8166453329d26bb3ec52ab036612591604c91a1
Atom <sarming@126.com>
2022-06-24 16:57:44 +0800

    hive create table

--- src/main/java/com/atom/hive/jdbc/CreateTableDemo.java
+++ src/main/java/com/atom/hive/jdbc/CreateTableDemo.java
@@ -14,8 +14,9 @@ public class CreateTableDemo {
 
     public static void main(String[] args) throws Exception {
         Class.forName("org.apache.hive.jdbc.HiveDriver");
-        Connection connection = DriverManager.getConnection("jdbc:hive2://xxx:10000/testschema", "", "");
-        PreparedStatement ppst = connection.prepareStatement("create table testschema.table001(id int,name string,age int )");
+//        Connection connection = DriverManager.getConnection("jdbc:hive2://xxx:10000/default?mapreduce.job.queuename=default;hive.cli.print.header=false", "", "");
+        Connection connection = DriverManager.getConnection("jdbc:hive2://xx:10000/default", "", "");
+        PreparedStatement ppst = connection.prepareStatement("create table default.table003(id int,name string,age int )");
         ppst.execute();
         ppst.close();
         connection.close();
